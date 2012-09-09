2b370bef6cd2116569dd49f626cc7b51823d14f7
Alvaro Cabrera <pateketrueke@gmail.com>
2012-09-08 19:47:30 -0500

    Reconfigura el entorno para servir en producción; fix

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <?php
 
-require 'vendor/autoload.php';
-require 'application.php';
+$loader = require 'vendor/autoload.php';
+$loader->add('Grocery', __DIR__.'/requires/grocery/lib/');
 
+Broil\Config::set('rewrite', TRUE);
 Broil\Config::set('request_uri', ! empty($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '/');
-Broil\Config::set('request_method', ! empty($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'HEAD');
+
+$_method = ! empty($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'HEAD';
+
+if ( ! empty($_POST['_method'])) {
+  $_method = strtoupper($_POST['_method']);
+}
+
+Broil\Config::set('request_method', $_method);
+
+Tailor\Config::set('cache_dir', '/tmp');
+Tailor\Config::set('views_dir', 'application/includes');
+
+Tailor\Base::initialize();
+
+require 'application/functions.php';
+require 'application/bootstrap.php';
 
 if ($action = Broil\Routing::run()) {
+  $headers = array();
+
   ob_start();
   $test = call_user_func_array($action['to'], array_values($action['params']));
   $text = ob_get_clean();
@@ -14,10 +32,19 @@ if ($action = Broil\Routing::run()) {
   if (is_numeric($test)) {
     $status = $test;
   } elseif (is_array($test)) {
-    @list($status, $headers) = $test;
+    switch (sizeof($test)) {
+      case 3;
+        @list($status, $headers, $text) = $test;
+      break;
+      case 2;
+        @list($status, $headers) = $test;
+      break;
+      default;
+        $status = array_shift($test);
+      break;
+    }
   } else {
     $status = 200;
-    $headers = array();
   }
 
   $params = array($status, $headers ?: array(), $text);
