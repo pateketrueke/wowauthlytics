7cb4a3d56f094d5905c5138d9a2be69dff19375b
Alvaro Cabrera <pateketrueke@gmail.com>
2012-09-09 09:36:17 -0500

    Estrategia para mailchimp; cambia dependencia de API

---
+++ application/providers/mailchimp/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once('libraries/mailchimp/MC_OAuth2Client.php');
+require_once('libraries/mailchimp/MC_RestClient.php');
+require_once('libraries/mailchimp/miniMCAPI.class.php');
+
+$client = new MC_OAuth2Client();
+$session = $client->getSession();
+
+if ( ! $session) {
+  echo '<a href="' . $client->getLoginUri() . '">Login</a>';
+} else {
+  $rest = new MC_RestClient($session);
+  $data = $rest->getMetadata();
+
+  return array('acces_token' => $session['access_token'], 'dc' => $data['dc']);
+}
